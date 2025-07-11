1351ca0b68144ea76c114e7d4e4fe64bfd277245
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-07-11 13:58:52 +0800

    feat: add Ticket class

---
+++ ZooTicketing/src/main/java/org/example/model/Ticket.java
@@ -0,0 +1,25 @@
+package org.example.model;
+
+public class Ticket {
+    private String code;
+    private String type;
+    private double price;
+
+    public Ticket(String code, String type, double price) {
+        this.code = code;
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
