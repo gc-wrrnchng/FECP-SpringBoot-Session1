a7009406223ba6fded8f8371b77971d8ddadd98d
Jahren Hans Dayag <jahrenhans.dayag@GXCHLAP00673.local>
2025-07-11 12:36:03 +0800

    Added Person Class and Constructor

---
+++ ZooAdministrationModule/src/main/java/org/example/Person.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Person {
+
+    private String name;
+    private String location;
+
+    public Person(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+
+}
