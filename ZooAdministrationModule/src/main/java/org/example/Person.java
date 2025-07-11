5036f204e4af7114db7d868beefbceefb228b3a8
Jahren Hans Dayag <jahrenhans.dayag@GXCHLAP00673.local>
2025-07-11 12:47:37 +0800

    Added getters and setters for Person Class

--- ZooAdministrationModule/src/main/java/org/example/Person.java
+++ ZooAdministrationModule/src/main/java/org/example/Person.java
@@ -10,5 +10,27 @@ public class Person {
         this.location = location;
     }
 
+    // Getters
+    public String getName() {
+        return name;
+    }
 
+    public String getLocation() {
+        return location;
+    }
+
+    // Setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Location: " + location;
+    }
 }
