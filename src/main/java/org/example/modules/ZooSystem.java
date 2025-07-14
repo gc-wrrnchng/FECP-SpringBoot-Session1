8ea477e1ecb146abc6fa41aedca5e7cf70b2695a
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-07-14 16:35:06 +0800

    chore: relocate files

---
+++ src/main/java/org/example/modules/ZooSystem.java
@@ -0,0 +1,72 @@
+package org.example.modules;
+
+import org.example.models.animals.*;
+
+import java.util.*;
+
+public class ZooSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Setting up the zoo
+        Zoo zooVisitor = getZoo();
+        //
+
+        int option;
+        do {
+            System.out.println("\nWhat would you like to do?");
+            System.out.println("1. Visit Enclosure");
+            System.out.println("2. Visit Shop");
+            System.out.println("3. Visit Hospital");
+            System.out.println("4. Leave Zoo");
+            System.out.print("Choose an option: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1: {
+                    zooVisitor.visitEnclosures();
+                    break;
+                }
+                case 2: {
+                    zooVisitor.visitShops();
+                    break;
+                }
+                case 3: {
+                    zooVisitor.visitHospital();
+                    break;
+                }
+                case 4: {
+                    System.out.println("=== Thank you for visiting the Zoo! ===");
+                    break;
+                }
+                default: {
+                    System.out.println("Not a valid option. Try again.");
+                }
+            }
+
+        } while (option != 4);
+    }
+
+    private static Zoo getZoo() {
+        List<Animal> animals = new ArrayList<>();
+        Animal elephant = new Elephant("Dumbo", "Pachyderm Enclosure");
+        Animal lion = new Lion("Leo", "Feline Enclosure");
+        Animal owl = new Owl("Hoot", "Bird Enclosure");
+        Animal tiger = new Tiger("Tigress", "Feline Enclosure");
+        Animal parrot = new Parrot("Parakeet", "Bird Enclosure");
+
+        tiger.setHealthy(false);
+        parrot.setHealthy(false);
+
+        animals.add(elephant);
+        animals.add(lion);
+        animals.add(owl);
+        animals.add(tiger);
+        animals.add(parrot);
+
+        Zoo zooVisitor = new Zoo(animals);
+        return zooVisitor;
+    }
+
+}
