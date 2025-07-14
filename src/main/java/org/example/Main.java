2539f8d9b89f2eddfa3affd8a60b0c060b0221c5
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-07-14 16:07:41 +0800

    fix: remove line error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        
-
         Scanner scanner = new Scanner(System.in);
 
         // Setting up the zoo
