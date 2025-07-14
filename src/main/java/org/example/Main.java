d06bb42705d092561e523ef733837cecd166f243
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-07-14 16:07:12 +0800

    refactor: add return type to TicketSystem.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        
+
         Scanner scanner = new Scanner(System.in);
 
         // Setting up the zoo
